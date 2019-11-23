75fed473e33e03fad274cd15447c2ff9a3c01086
Philipp <p.kundratitz@hotmail.com>
2019-11-23 02:17:16 +0100

    logging settings

--- leaderboard/settings.py
+++ leaderboard/settings.py
@@ -93,6 +93,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
